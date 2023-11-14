069d052bb35d601e9a30ebb5056cd0ebdef9ffbe
シン <98733853+kensham2003@users.noreply.github.com>
2023-11-14 18:30:53 +0900

    game loop in C# side(in progress)

---
+++ GameEngine/GameLoop/game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameEngine.Component;
+
+namespace GameEngine.GameLoop
+{
+    public class Game
+    {
+        public List<GameObject> m_gameObjects;
+
+        public void Init() { }
+
+        public void Uninit() { }
+
+        public void Update(TimeSpan gameTime) { }
+
+        public void AddGameObject(GameObject gameObject)
+        {
+            m_gameObjects.Add(gameObject);
+        }
+
+        public void RemoveGameObject(GameObject gameObject)
+        {
+            m_gameObjects.Remove(gameObject);
+        }
+    }
+}
---
+++ GameEngine/GameLoop/gameLoop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.GameLoop
+{
+    class gameLoop
+    {
+        private Game m_game;
+
+        public bool m_running { get; private set; }
+
+        public void Load(Game game) { m_game = game; }
+
+        public async void Start()
+        {
+            if (m_game == null)
+                throw new ArgumentException("Game not loaded");
+
+            m_game.Init();
+
+            m_running = true;
+
+            DateTime prevTime = DateTime.Now;
+
+            while (m_running)
+            {
+                TimeSpan gameTime = DateTime.Now - prevTime;
+
+                prevTime = prevTime + gameTime;
+
+                m_game.Update(gameTime);
+
+                await Task.Delay(8);
+            }
+        }
+
+        public void Stop()
+        {
+            m_running = false;
+            m_game?.Uninit();
+        }
+    }
+}
